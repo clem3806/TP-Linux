f846588062fcc6e8467572861e19c35f72b6985c
clem3806 <clement.cernesson.ad@gmail.com>
2022-12-12 14:46:29 +0000

    log.sh complet

--- log.sh
+++ log.sh
@@ -1,10 +1,8 @@
 A=1
 while [ $A -le $1 ]; do
 	E="$(mktemp)"
-	echo $E
 	F="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c10)"
-	echo $F
 	sudo mv $E /var/log/exercice/recent_logs/$F
-	cat /dev/urandom | tr -dc '[:alpha:]' | head -c10 >> /var/log/exercice/recent_logs/$F
+	cat /dev/urandom | tr -dc '[:alpha:]' | head -c10 > /var/log/exercice/recent_logs/$F
 	A=$[$A + 1]
 done

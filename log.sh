f6826229598bfa64ef3c36b72c760cf34589dd2f
clem3806 <clement.cernesson.ad@gmail.com>
2022-12-12 14:34:45 +0000

    aléatoire résolu. Maintenant, il est temps de trouver comment vérifier que le nom est unique

--- log.sh
+++ log.sh
@@ -2,6 +2,9 @@ A=1
 while [ $A -le $1 ]; do
 	E="$(mktemp)"
 	echo $E
-	sudo mv $E /var/log/exercice/recent_logs
+	F="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c10)"
+	echo $F
+	sudo mv $E /var/log/exercice/recent_logs/$F
+	cat /dev/urandom | tr -dc '[:alpha:]' | head -c10 >> /var/log/exercice/recent_logs/$F
 	A=$[$A + 1]
 done

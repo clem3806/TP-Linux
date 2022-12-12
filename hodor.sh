6bca3eefb52f5eb19ea6039b2df98170934fa486
clem3806 <clement.cernesson.ad@gmail.com>
2022-12-12 15:21:24 +0000

    HODOR FONCTIONNE

--- hodor.sh
+++ hodor.sh
@@ -1,1 +1,4 @@
-echo "HODOOOR !!!"
+echo "HODOOOR !!!" | wall
+E="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c10)"
+sudo tar -zcvf /var/log/exercice/archives/$E.tar.gz /var/log/exercice/recent_logs
+sudo rm /var/log/exercice/recent_logs/*

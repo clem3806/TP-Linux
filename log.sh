fc260475e9cc049ae01920163d762e2e9d3894a7
clem3806 <71267364+clem3806@users.noreply.github.com>
2022-12-13 11:37:29 +0100

    Commentaires sur Log.sh

--- log.sh
+++ log.sh
@@ -1,8 +1,12 @@
 A=1
 while [ $A -le $1 ]; do
+	#creation du fichier
 	E="$(mktemp)"
+	#generation du nom compose de caracteres aleatoires
 	F="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c10)"
+	#deplacement du fichier et changement de son nom
 	sudo mv $E /var/log/exercice/recent_logs/$F
+	#remplissage du fichier avec dix caracteres aleatoires
 	cat /dev/urandom | tr -dc '[:alpha:]' | head -c10 > /var/log/exercice/recent_logs/$F
 	A=$[$A + 1]
 done

a8cd795c0bdc198e054d31d658e9d6dc1c61b231
clem3806 <71267364+clem3806@users.noreply.github.com>
2022-12-13 11:40:48 +0100

    Commentaires sur Hodor

--- hodor.sh
+++ hodor.sh
@@ -1,7 +1,12 @@
 A="$(df -i / | cut -c 40-42 | cut -d ' ' -f 2 | tail -1)"
-if [ $A -ge 20 ]; then
+#verification de la quantite d'inodes utilisees
+if [ $A -ge 15 ]; then
+	#HODOOOR!
 	echo 'HODOOOR!!!'
+	#generation du nom de l'archive
 	E="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c10)"
+	#archivage des fichiers et anihilation du mur de texte genere
 	sudo tar -zcvf /var/log/exercice/archives/$E.tar.gz /var/log/exercice/recent_logs >> /dev/null
+	#supression des fichiers dans le dossier original
 	sudo rm /var/log/exercice/recent_logs/*
 fi
